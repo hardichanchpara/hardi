02882a667ceaaae8bc0fc704c66b94e5945d50d1
hardichanchpara <131763035+hardichanchpara@users.noreply.github.com>
2023-05-01 15:26:26 +0530

    Add files via upload

---
+++ 28APRIL2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   clrscr();
+   int a,b;
+   printf("the number A= ");
+   scanf("%d",&a);
+   printf("thr number B= ");
+   scanf("%d",&b);
+   if(a<b)
+   {
+   printf("A is minimum");
+   }
+   else
+   {
+   printf("B is minimum");
+   }
+   getch();
+}
+
